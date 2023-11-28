7a681311dcca1260cfaa35bd1b064e6c12a99a5a
S-Kos <103041224+S-Kos@users.noreply.github.com>
2023-11-28 15:49:53 +0100

    Css nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,20 @@
 <nav>
+<img src="images/logo.png" alt="Logo">
     <ul>
-        <li><a class="active" href="../index.php">Home</a></li>
-        <li><a href="#news">File</a></li>
-        <li><a href="#contact">View</a></li>
-        <li><a href="#about">Help</a></li>
-        <li><a href="../paginas/login-page.php">Inlog/Uitlog</a></li>
-        <li><a href="#about">Account</a></li>        
+    <div class="dropdown">
+  <button class="btn">Taal</button>
+  <div class="dropdown-content">
+  <a href="#">Link 1</a>
+  <a href="#">Link 2</a>
+  <a href="#">Link 3</a>
+  </div>
+</div>
+
+        <button class="btn navbut"><a class="btn" href="../index.php">Home</a></button>
+        <button class="btn navbut"><a class="btn" href="#news">File</a></button>
+        <button class="btn navbut"><a class="btn" href="#contact">View</a></button>
+        <button class="btn navbut"><a class="btn" href="#about">Help</a></button>
+        <button class="btn navbut"><a class="btn" href="../paginas/login-page.php">Inlog/Uitlog</a></button>
+        <button class="btn navbut"><a class="btn" href="#about">Account</a></button>        
     </ul>
 </nav>
