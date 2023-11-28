2fff080056c56e2cae2de0886c09df4360ac7e9c
S-Kos <103041224+S-Kos@users.noreply.github.com>
2023-11-28 12:00:21 +0100

    Styling footer

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+    <div class="fbox">
+        <p>1-800-243-0000</p><br>
+        <p>info@uitzendbureauxxl.nl</p>
+    </div>
+    <div class="fbox">
+        <p>Uitzendbureau XXL</p>
+    </div>
+    <div class="fbox">
+        <p>Links</p>
+    </div>
+</div>
