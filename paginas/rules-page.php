9941962fce78e4b9a0c9a087476ef846e4d86dff
Tomeelman <114242018+TomEelman@users.noreply.github.com>
2023-11-30 11:59:31 +0100

    Huistregels

--- paginas/rules-page.php
+++ paginas/rules-page.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <pnk rel="stylesheet" type="text/css" href="../style/pages.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
@@ -9,7 +11,17 @@
     <?php include("../components/session-start.php") ?>
     <?php include("../includes/navbar.php") ?>
 </header>
+
 <body>
-    
+    <h1 style="text-align: center;">Regels van de website</h1>
+
+    <div class="rules">
+        <ul>
+            <p style="text-align: center;">Lorem ipsum dolor sit amet, consectetur adipiscing ept.</p>
+            <p style="text-align: center;">Sed auctor pbero eu mauris dictum, ut consectetur lorem faucibus.</p>
+            <p style="text-align: center;">Vivamus vestibulum augue at malesuada posuere.</p>
+        </ul>
+    </div>
 </body>
+
 </html>
